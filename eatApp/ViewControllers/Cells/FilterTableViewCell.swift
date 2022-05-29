5973aa9821d42e88cd816d3cbd5abee1c8831c2d
Samad <asamad5353@gmail.com>
2022-05-29 14:35:37 +0400

    filter vc implemented

---
+++ eatApp/ViewControllers/Cells/FilterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FilterTableViewCell.swift
+//  eatApp
+//
+//  Created by Abdul Samad on 5/29/22.
+//
+
+import UIKit
+
+class FilterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- eatApp/ViewControllers/FilterViewController.swift
+++ eatApp/ViewControllers/FilterViewController.swift
@@ -9,8 +9,74 @@ import UIKit
 
 class FilterViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var priceSelectionCollection: [UIControl]!
+    @IBOutlet var priceSelectionLabelCollection: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Filter"
+        setupView()
     }
+    
+    private func setupView() {
+        for each in priceSelectionCollection {
+            each.layer.cornerRadius = 5
+            each.backgroundColor = .clear
+            each.layer.borderWidth = 1
+            each.layer.borderColor = UIColor.separator.cgColor
+            if each.tag == 0 {
+                each.backgroundColor = UIColor(named: "green")
+                priceSelectionLabelCollection[0].textColor = .white
+            }
+        }
+    }
+}
+
+extension FilterViewController {
+    
+    @IBAction func cancelButtonClicked(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func priceSelectionButtonClicked(_ sender: UIControl) {
+        for each in priceSelectionCollection {
+            if each.tag == sender.tag {
+                sender.backgroundColor = UIColor(named: "green")
+            } else {
+                each.backgroundColor = .clear
+                each.layer.borderWidth = 1
+                each.layer.borderColor = UIColor.separator.cgColor
+            }
+        }
+        
+        for each in priceSelectionLabelCollection {
+            if each.tag == sender.tag {
+                each.textColor = .white
+            } else {
+                each.textColor = UIColor(named: "priceDeselect")
+            }
+        }
+    }
+}
+
+extension FilterViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FilterTableViewCell", for: indexPath) as? FilterTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "FILTER BY"
+    }
+    
+    
 }
